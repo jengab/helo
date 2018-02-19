ff5bba17f7db65c859e4225c7d7051479696f527
Gábor Jenei <jengab@elte.hu>
2018-02-19 15:04:58 +0100

    Correcting indentation and added some check to Makefile

--- helo_offline/src/source/cluster.cpp
+++ helo_offline/src/source/cluster.cpp
@@ -9,28 +9,28 @@
  * @param[in] dict A set that contains the words used in the cluster
  */
 Cluster::Cluster(const std::shared_ptr<ListOfLines> lines,const DictionaryPtr dict):Content(lines),dict(dict),id(0),TotalLineLen(0){
-	TotalLineCount=Content->size();
-	CalcStatistics();
+    TotalLineCount=Content->size();
+    CalcStatistics();
 }
 
 /**
-* @return The column's number to split on, -1 if there is no ideal column
-*/
+ * @return The column's number to split on, -1 if there is no ideal column
+ */
 int Cluster::getSplit(){
-	double max=-1;
-	int pos=-1;
-	for(size_t ind=0;ind<MaxLineLen;++ind){
-		size_t NoDistinctValues=Values[ind].size();
-		if(NoDistinctValues>1 &&
-				max<(double)FilledNonNumColumns[ind]/NoDistinctValues &&
-				(double)FilledNonNumColumns[ind]/Content->size()>PERCENT_OF_FILL
-			){
-			max=(double)FilledNonNumColumns[ind]/NoDistinctValues;
-			pos=ind;
-		}
-	}
+    double max=-1;
+    int pos=-1;
+    for(size_t ind=0;ind<MaxLineLen;++ind){
+        size_t NoDistinctValues=Values[ind].size();
+        if(NoDistinctValues>1 &&
+                max<(double)FilledNonNumColumns[ind]/NoDistinctValues &&
+                (double)FilledNonNumColumns[ind]/Content->size()>PERCENT_OF_FILL
+          ){
+            max=(double)FilledNonNumColumns[ind]/NoDistinctValues;
+            pos=ind;
+        }
+    }
 
-	return pos;
+    return pos;
 }
 
 /**
@@ -40,84 +40,84 @@ int Cluster::getSplit(){
  * @throws std::invalid_argument if the cluster can't be split yet (there is no proper split position)
  */
 void Cluster::Split(ListOfClusters& ClusterList){
-	int Position=getSplit();
-	if(Position==-1) throw std::invalid_argument("The cluster is not splitable yet!\n");
-	std::map<std::shared_ptr<TokenDescriptor>,std::shared_ptr<ListOfLines>,WordComparator> Clusters;
+    int Position=getSplit();
+    if(Position==-1) throw std::invalid_argument("The cluster is not splitable yet!\n");
+    std::map<std::shared_ptr<TokenDescriptor>,std::shared_ptr<ListOfLines>,WordComparator> Clusters;
 
-	for(ArrayOfWords& ActLine:*Content){
-		std::shared_ptr<TokenDescriptor> ClusterLabel;
+    for(ArrayOfWords& ActLine:*Content){
+        std::shared_ptr<TokenDescriptor> ClusterLabel;
 
-		try{
-			std::shared_ptr<TokenDescriptor> TokDesc=ActLine.at(Position);
-			if(TokDesc->TypeOfToken==Number){
-				ClusterLabel=*(dict->find(std::make_shared<TokenDescriptor>(L"+d")));
-			}
-			else{
-				ClusterLabel=TokDesc;
-			}
-		}
-		catch(const std::out_of_range&){
-			ClusterLabel=*(dict->find(std::make_shared<TokenDescriptor>(L"+n")));
-		}
+        try{
+            std::shared_ptr<TokenDescriptor> TokDesc=ActLine.at(Position);
+            if(TokDesc->TypeOfToken==Number){
+                ClusterLabel=*(dict->find(std::make_shared<TokenDescriptor>(L"+d")));
+            }
+            else{
+                ClusterLabel=TokDesc;
+            }
+        }
+        catch(const std::out_of_range&){
+            ClusterLabel=*(dict->find(std::make_shared<TokenDescriptor>(L"+n")));
+        }
 
-		try{
-			Clusters.at(ClusterLabel)->push_back(ActLine);
-		}
-		catch(std::out_of_range&){
-			Clusters.insert(std::make_pair(ClusterLabel,std::make_shared<ListOfLines>()));
-			Clusters[ClusterLabel]->push_back(ActLine);
-		}
-	}
+        try{
+            Clusters.at(ClusterLabel)->push_back(ActLine);
+        }
+        catch(std::out_of_range&){
+            Clusters.insert(std::make_pair(ClusterLabel,std::make_shared<ListOfLines>()));
+            Clusters[ClusterLabel]->push_back(ActLine);
+        }
+    }
 
-	for(const auto& ActEntry:Clusters){
-		ClusterList.push_back(Cluster(ActEntry.second,dict));
-	}
+    for(const auto& ActEntry:Clusters){
+        ClusterList.push_back(Cluster(ActEntry.second,dict));
+    }
 }
 
 void Cluster::CalcStatistics(){
-	double AvgLen=0;
-	MaxLineLen=0;
-	FilledColumns.clear();
-	FilledNonNumColumns.clear();
-	Values.clear();
+    double AvgLen=0;
+    MaxLineLen=0;
+    FilledColumns.clear();
+    FilledNonNumColumns.clear();
+    Values.clear();
 
-	for(const ArrayOfWords& ActLine:*Content){
-		if(ActLine.size()>MaxLineLen){
-			MaxLineLen=ActLine.size();
-			Values.resize(MaxLineLen);
-			FilledColumns.resize(MaxLineLen);
-			FilledNonNumColumns.resize(MaxLineLen);
-		}
-		AvgLen+=ActLine.size();
+    for(const ArrayOfWords& ActLine:*Content){
+        if(ActLine.size()>MaxLineLen){
+            MaxLineLen=ActLine.size();
+            Values.resize(MaxLineLen);
+            FilledColumns.resize(MaxLineLen);
+            FilledNonNumColumns.resize(MaxLineLen);
+        }
+        AvgLen+=ActLine.size();
 
-		for(size_t ActPos=0;ActPos<ActLine.size();++ActPos){
-			Values[ActPos].insert(ActLine[ActPos]);
-			if(ActLine[ActPos]->TypeOfToken!=Number) FilledNonNumColumns[ActPos]++;
-			FilledColumns[ActPos]++;
-		}
-	}
+        for(size_t ActPos=0;ActPos<ActLine.size();++ActPos){
+            Values[ActPos].insert(ActLine[ActPos]);
+            if(ActLine[ActPos]->TypeOfToken!=Number) FilledNonNumColumns[ActPos]++;
+            FilledColumns[ActPos]++;
+        }
+    }
 
-	size_t CommonWordCounter=0;
-	for(size_t i=0;i<Values.size();++i){
-		if(Values[i].size()==1 && FilledColumns[i]==Content->size()) CommonWordCounter++;
-	}
+    size_t CommonWordCounter=0;
+    for(size_t i=0;i<Values.size();++i){
+        if(Values[i].size()==1 && FilledColumns[i]==Content->size()) CommonWordCounter++;
+    }
 
-	if(TotalLineLen==0) TotalLineLen=AvgLen;
-	AvgLen/=Content->size();
+    if(TotalLineLen==0) TotalLineLen=AvgLen;
+    AvgLen/=Content->size();
 
-	if(Content->size()==0 || MaxLineLen==0){
-		goodness=1;
-	}
-	else{
-		goodness=(double)CommonWordCounter/AvgLen;
-	}
+    if(Content->size()==0 || MaxLineLen==0){
+        goodness=1;
+    }
+    else{
+        goodness=(double)CommonWordCounter/AvgLen;
+    }
 }
 
 /**
  * @return The calculated goodness (a measure of similarity for lines contained in the cluster) value
  */
 double Cluster::getGoodness(){
-	return goodness;
+    return goodness;
 }
 
 /**
@@ -127,57 +127,57 @@ double Cluster::getGoodness(){
  * @return The relative goodness of the clusters (it is a measure of closeness here).
  */
 double Cluster::getGoodness(Cluster& other){
-	if(Content->empty() ||other.Content->empty()) return 0;
-	size_t CommonWordCounter=0;
-	ArrayOfWords& this_template=(*(Content->begin()));
-	ArrayOfWords& other_template=(*(other.Content->begin()));
-	size_t this_length=this_template.size();
-	size_t other_length=other_template.size();
-	double AvgLineLen=(double)(this_length+other_length)/2;
+    if(Content->empty() ||other.Content->empty()) return 0;
+    size_t CommonWordCounter=0;
+    ArrayOfWords& this_template=(*(Content->begin()));
+    ArrayOfWords& other_template=(*(other.Content->begin()));
+    size_t this_length=this_template.size();
+    size_t other_length=other_template.size();
+    double AvgLineLen=(double)(this_length+other_length)/2;
 
-	for(size_t ActPos=0;ActPos<this_length && ActPos<other_length;++ActPos){
-		if(this_template[ActPos]->TokenString==other_template[ActPos]->TokenString) CommonWordCounter++;
-	}
+    for(size_t ActPos=0;ActPos<this_length && ActPos<other_length;++ActPos){
+        if(this_template[ActPos]->TokenString==other_template[ActPos]->TokenString) CommonWordCounter++;
+    }
 
-	return (double)CommonWordCounter/AvgLineLen;
+    return (double)CommonWordCounter/AvgLineLen;
 }
 
 /**
  * @return A template that describes the cluster
  */
 ArrayOfWords Cluster::getTemplate(){
-	ArrayOfWords Template;
+    ArrayOfWords Template;
 
-	std::vector<wordtype> AggregatedType(MaxLineLen,Number);
-	for(const ArrayOfWords& ActLine:*Content){
-		size_t LineLen=ActLine.size();
-		for(size_t i=0;i<LineLen;++i){
-			if(ActLine[i]->TypeOfToken!=Number) AggregatedType[i]=ActLine[i]->TypeOfToken;
-		}
-	}
+    std::vector<wordtype> AggregatedType(MaxLineLen,Number);
+    for(const ArrayOfWords& ActLine:*Content){
+        size_t LineLen=ActLine.size();
+        for(size_t i=0;i<LineLen;++i){
+            if(ActLine[i]->TypeOfToken!=Number) AggregatedType[i]=ActLine[i]->TypeOfToken;
+        }
+    }
 
-	ArrayOfWords& FirstLine=(*(Content->begin()));
-	for(size_t WordCounter=0;WordCounter<MaxLineLen;++WordCounter){
-		if(FilledColumns[WordCounter]==Content->size()){ //isn't it +n
-			if(Values[WordCounter].size()>1){ //is it constant?
-				if(AggregatedType[WordCounter]!=Number){
-					Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"*"))));
-				}
-				else{
-					Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"+d"))));
-				}
+    ArrayOfWords& FirstLine=(*(Content->begin()));
+    for(size_t WordCounter=0;WordCounter<MaxLineLen;++WordCounter){
+        if(FilledColumns[WordCounter]==Content->size()){ //isn't it +n
+            if(Values[WordCounter].size()>1){ //is it constant?
+                if(AggregatedType[WordCounter]!=Number){
+                    Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"*"))));
+                }
+                else{
+                    Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"+d"))));
+                }
 
-			}
-			else{
-				Template.push_back(FirstLine[WordCounter]);
-			}
-		}
-		else{
-			Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"+n"))));
-			break;
-		}
-	}
-	return Template;
+            }
+            else{
+                Template.push_back(FirstLine[WordCounter]);
+            }
+        }
+        else{
+            Template.push_back(*(dict->find(std::make_shared<TokenDescriptor>(L"+n"))));
+            break;
+        }
+    }
+    return Template;
 }
 
 /**
@@ -185,9 +185,9 @@ ArrayOfWords Cluster::getTemplate(){
  * calculated from the former content.
  */
 void Cluster::compressToTemplate(){
-	ArrayOfWords Template=getTemplate();
-	Content->clear();
-	Content->push_back(Template);
+    ArrayOfWords Template=getTemplate();
+    Content->clear();
+    Content->push_back(Template);
 }
 
 /**
@@ -198,48 +198,48 @@ void Cluster::compressToTemplate(){
  * totally deleted
  */
 void Cluster::join(Cluster& other){
-	size_t SumLen=0;
-	for(const ArrayOfWords& ActLine:*other.Content){
-		SumLen+=ActLine.size();
-		Content->push_back(ActLine);
-	}
+    size_t SumLen=0;
+    for(const ArrayOfWords& ActLine:*other.Content){
+        SumLen+=ActLine.size();
+        Content->push_back(ActLine);
+    }
 
-	TotalLineLen+=SumLen;
-	TotalLineCount+=other.TotalLineCount;
-	other.Content->clear();
+    TotalLineLen+=SumLen;
+    TotalLineCount+=other.TotalLineCount;
+    other.Content->clear();
 
-	ListOfLines::iterator it=Content->begin();
-	ArrayOfWords& FirstLine=(*it++);
-	ArrayOfWords& SecondLine=(*it);
-	ArrayOfWords Template;
-	for(size_t i=0;i<FirstLine.size() && i<SecondLine.size();++i){
-		if(FirstLine[i]->TokenString==L"+n" || SecondLine[i]->TokenString==L"+n"){
-			Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+n")));
-			Content->clear();
-			Content->push_back(Template);
-			return;
-		}
+    ListOfLines::iterator it=Content->begin();
+    ArrayOfWords& FirstLine=(*it++);
+    ArrayOfWords& SecondLine=(*it);
+    ArrayOfWords Template;
+    for(size_t i=0;i<FirstLine.size() && i<SecondLine.size();++i){
+        if(FirstLine[i]->TokenString==L"+n" || SecondLine[i]->TokenString==L"+n"){
+            Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+n")));
+            Content->clear();
+            Content->push_back(Template);
+            return;
+        }
 
-		wordtype AggregateType=Number;
-		if(FirstLine[i]->TypeOfToken!=Number || SecondLine[i]->TypeOfToken!=Number) AggregateType=Word;
-		if(FirstLine[i]->TokenString==SecondLine[i]->TokenString){ //insert the string
-			Template.push_back(FirstLine[i]);
-		}
-		else{
-			if(AggregateType==Number){ //insert +d
-				Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+d")));
-			}
-			else{ //insert *
-				Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"*")));
-			}
-		}
-	}
+        wordtype AggregateType=Number;
+        if(FirstLine[i]->TypeOfToken!=Number || SecondLine[i]->TypeOfToken!=Number) AggregateType=Word;
+        if(FirstLine[i]->TokenString==SecondLine[i]->TokenString){ //insert the string
+            Template.push_back(FirstLine[i]);
+        }
+        else{
+            if(AggregateType==Number){ //insert +d
+                Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+d")));
+            }
+            else{ //insert *
+                Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"*")));
+            }
+        }
+    }
 
-	if(FirstLine.size()!=SecondLine.size()){ //push_back +n
-		Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+n")));
-	}
-	Content->clear();
-	Content->push_back(Template);
+    if(FirstLine.size()!=SecondLine.size()){ //push_back +n
+        Template.push_back(*dict->find(std::make_shared<TokenDescriptor>(L"+n")));
+    }
+    Content->clear();
+    Content->push_back(Template);
 }
 
 /**
@@ -250,45 +250,45 @@ void Cluster::join(Cluster& other){
  * @return A reference to the output stream that was used for writing
  */
 std::wostream& operator<<(std::wostream& o,const Cluster& c){
-	pugi::xml_document doc;
+    pugi::xml_document doc;
 
-	pugi::xml_node ClustNode=doc.append_child(LogParser::ClusterNodeName);
-	ClustNode.append_attribute(LogParser::IdAttributeName)=c.id;
-	ClustNode.append_attribute(LogParser::GoodnessAttributeName)=c.goodness;
-	ClustNode.append_attribute(LogParser::AvgLenAttributeName)=c.getAvgLen();
+    pugi::xml_node ClustNode=doc.append_child(LogParser::ClusterNodeName);
+    ClustNode.append_attribute(LogParser::IdAttributeName)=c.id;
+    ClustNode.append_attribute(LogParser::GoodnessAttributeName)=c.goodness;
+    ClustNode.append_attribute(LogParser::AvgLenAttributeName)=c.getAvgLen();
 
-	for(const std::shared_ptr<TokenDescriptor> ActWord:(*c.Content->begin())){
-		pugi::xml_node TokenNode=ClustNode.append_child(LogParser::TokenNodeName);
-		TokenNode.append_attribute(LogParser::TokenValAttributeName)=ActWord->TokenString.c_str();
-		TokenNode.append_attribute(LogParser::TokenTypeAttributeName)=ActWord->TypeOfToken;
-	}
+    for(const std::shared_ptr<TokenDescriptor> ActWord:(*c.Content->begin())){
+        pugi::xml_node TokenNode=ClustNode.append_child(LogParser::TokenNodeName);
+        TokenNode.append_attribute(LogParser::TokenValAttributeName)=ActWord->TokenString.c_str();
+        TokenNode.append_attribute(LogParser::TokenTypeAttributeName)=ActWord->TypeOfToken;
+    }
 
-	doc.print(o);
-	return o;
+    doc.print(o);
+    return o;
 }
 
 /**
-* This method implements cluster output to a SQLite database.
-* The database must be already opened for writing before using this method
-*
-* @param[in,out] db A reference to the Database object, which implements the database API
-* @param[in] c The cluster to write to the database
-* @return Reference to the provided Database object
-*/
+ * This method implements cluster output to a SQLite database.
+ * The database must be already opened for writing before using this method
+ *
+ * @param[in,out] db A reference to the Database object, which implements the database API
+ * @param[in] c The cluster to write to the database
+ * @return Reference to the provided Database object
+ */
 SQLite::Database& operator<<(SQLite::Database& db,const Cluster& c){
-	std::ostringstream query;
-	try{
-		query << "INSERT INTO clusters VALUES (NULL,\"";
-		for(const std::shared_ptr<TokenDescriptor> ActWord:(*c.Content->begin())){
-			query << boost::spirit::to_utf8<wchar_t>(ActWord->TokenString) << " ";
-		}
+    std::ostringstream query;
+    try{
+        query << "INSERT INTO clusters VALUES (NULL,\"";
+        for(const std::shared_ptr<TokenDescriptor> ActWord:(*c.Content->begin())){
+            query << boost::spirit::to_utf8<wchar_t>(ActWord->TokenString) << " ";
+        }
 
-		query << "\"," << c.goodness << "," << c.getAvgLen() << ")";
-		db.exec(query.str().c_str());
-	}
-	catch(const SQLite::Exception& e){
-		std::wcerr << "An SQL error occured: " << e.what() << std::endl << "Query: " << query.str().c_str() << std::endl;
-	}
+        query << "\"," << c.goodness << "," << c.getAvgLen() << ")";
+        db.exec(query.str().c_str());
+    }
+    catch(const SQLite::Exception& e){
+        std::wcerr << "An SQL error occured: " << e.what() << std::endl << "Query: " << query.str().c_str() << std::endl;
+    }
 
-	return db;
+    return db;
 }
--- helo_online/src/source/ClusterTemplate.cpp
+++ helo_online/src/source/ClusterTemplate.cpp
@@ -4,79 +4,79 @@
 
 /**
 * This method tests if a log messages matches the represented cluster template
-* 
+*
 * @param[in] msg The syslog message to test
 * @return true if the message matches the template represented by the object, false otherwise
 */
 bool ClusterTemplate::match(const std::vector<TokenDescriptor>& msg) const{
-	for(size_t i=0;i<msg.size() && i<Template.size();++i){
-		if(Template[i].TokenString==L"*") continue; //Anything matches *
-		if(Template[i].TokenString==L"+d" && msg[i].TypeOfToken==Number) continue; //accept number matching
-		if(Template[i].TokenString==L"+n") return true;
-		if(msg[i].TokenString!=Template[i].TokenString) return false;
-	}
+  for(size_t i=0;i<msg.size() && i<Template.size();++i){
+    if(Template[i].TokenString==L"*") continue; //Anything matches *
+    if(Template[i].TokenString==L"+d" && msg[i].TypeOfToken==Number) continue; //accept number matching
+    if(Template[i].TokenString==L"+n") return true;
+    if(msg[i].TokenString!=Template[i].TokenString) return false;
+  }
 
-	return msg.size()==Template.size();
+  return msg.size()==Template.size();
 }
 
 /**
 * This method calculates the goodness that would be correct if we appended the
 * provided message to the represented cluster. This method does not change the cluster!
-* 
+*
 * @param[in] msg The message to try
 * @return The goodness value that is correct if msg was added to the cluster
 */
 double ClusterTemplate::getGoodness(const std::vector<TokenDescriptor>& msg) const{
-	double NewAvgLen=(AvgLen+msg.size())/2;
-	size_t CommonWordCounter=0;
+  double NewAvgLen=(AvgLen+msg.size())/2;
+  size_t CommonWordCounter=0;
 
-	for(size_t i=0;i<msg.size() && i<Template.size();++i){
-		if(Template[i].TokenString==L"+n") break;
-		if(Template[i].TokenString==msg[i].TokenString || //match for exact match
-			(Template[i].TokenString==L"+d" && msg[i].TypeOfToken==Number)){ //match for number
-			++CommonWordCounter;
-		}
-	}
+  for(size_t i=0;i<msg.size() && i<Template.size();++i){
+    if(Template[i].TokenString==L"+n") break;
+    if(Template[i].TokenString==msg[i].TokenString || //match for exact match
+      (Template[i].TokenString==L"+d" && msg[i].TypeOfToken==Number)){ //match for number
+      ++CommonWordCounter;
+    }
+  }
 
-	return CommonWordCounter/NewAvgLen;
+  return CommonWordCounter/NewAvgLen;
 }
 
 /**
 * This method adds a given line to the represented cluster, it calculates the
 * new goodness, average length and template values. It really modifies the object.
-* 
+*
 * @param[in] line The line to append to the cluster
 * @param[in] NewGoodness The new goodness value for the cluster. Use getGoodness to determine it!
 */
 void ClusterTemplate::join(const std::vector<TokenDescriptor>& line,double NewGoodness){
-	goodness=NewGoodness;
-	AvgLen=(AvgLen+line.size())/2;
+  goodness=NewGoodness;
+  AvgLen=(AvgLen+line.size())/2;
 
-	for(size_t i=0;i<Template.size() && i<line.size();++i){
-		if(Template[i].TokenString==L"+n") break;
-		if(Template[i].TokenString!=line[i].TokenString && Template[i].TokenString!=L"*"){ //constant tokens and * are not modified
-			if(line[i].TypeOfToken!=Number){  //received token is not a number => template only can be (*,Word)
-				Template[i].TypeOfToken=Word;
-				Template[i].TokenString=L"*";
-			}
-			else if(Template[i].TokenString!=L"+d"){ //received token is a number and the template is not +d
-				if(Template[i].TypeOfToken==Number){ //template is also number, so we can provide +d
-					Template[i].TokenString=L"+d";
-				}
-				else{ //template is not a number so we must set * and Word
-					Template[i].TokenString=L"*";
-				}
-			}
-		}
-	}
+  for(size_t i=0;i<Template.size() && i<line.size();++i){
+    if(Template[i].TokenString==L"+n") break;
+    if(Template[i].TokenString!=line[i].TokenString && Template[i].TokenString!=L"*"){ //constant tokens and * are not modified
+      if(line[i].TypeOfToken!=Number){  //received token is not a number => template only can be (*,Word)
+        Template[i].TypeOfToken=Word;
+        Template[i].TokenString=L"*";
+      }
+      else if(Template[i].TokenString!=L"+d"){ //received token is a number and the template is not +d
+        if(Template[i].TypeOfToken==Number){ //template is also number, so we can provide +d
+          Template[i].TokenString=L"+d";
+        }
+        else{ //template is not a number so we must set * and Word
+          Template[i].TokenString=L"*";
+        }
+      }
+    }
+  }
 
-	if(line.size()>Template.size()){
-		Template.push_back(TokenDescriptor(L"+n"));
-	}
-	else if(line.size()<Template.size()){
-		for(size_t i=Template.size();i>line.size()+1;--i) Template.pop_back();
-		Template[line.size()]=TokenDescriptor(L"+n");
-	}
+  if(line.size()>Template.size()){
+    Template.push_back(TokenDescriptor(L"+n"));
+  }
+  else if(line.size()<Template.size()){
+    for(size_t i=Template.size();i>line.size()+1;--i) Template.pop_back();
+    Template[line.size()]=TokenDescriptor(L"+n");
+  }
 }
 
 /**
@@ -84,16 +84,16 @@ void ClusterTemplate::join(const std::vector<TokenDescriptor>& line,double NewGo
 * (This is normally used on clusters that are not already present in the database)
 */
 std::string ClusterTemplate::getValueStr() const{
-	std::ostringstream values;
-	values << "INSERT INTO clusters VALUES(NULL,\"";
+  std::ostringstream values;
+  values << "INSERT INTO clusters VALUES(NULL,\"";
 
-	for(const TokenDescriptor& ActToken:Template){
-		values << boost::spirit::to_utf8<wchar_t>(ActToken.TokenString) << " ";
-	}
+  for(const TokenDescriptor& ActToken:Template){
+    values << boost::spirit::to_utf8<wchar_t>(ActToken.TokenString) << " ";
+  }
 
-	values << "\"," << goodness << "," << AvgLen << ")";
+  values << "\"," << goodness << "," << AvgLen << ")";
 
-	return values.str();
+  return values.str();
 }
 
 /**
@@ -102,14 +102,14 @@ std::string ClusterTemplate::getValueStr() const{
 * stored in the database)
 */
 std::string ClusterTemplate::getUpdateStr() const{
-	std::ostringstream update;
-	update << "UPDATE clusters SET goodness=" << goodness << ",AvgLen=" << AvgLen << ",template=\"";
+  std::ostringstream update;
+  update << "UPDATE clusters SET goodness=" << goodness << ",AvgLen=" << AvgLen << ",template=\"";
 
-	for(const TokenDescriptor& ActToken:Template){
-		update << boost::spirit::to_utf8<wchar_t>(ActToken.TokenString) << " ";
-	}
+  for(const TokenDescriptor& ActToken:Template){
+    update << boost::spirit::to_utf8<wchar_t>(ActToken.TokenString) << " ";
+  }
 
-	update << "\"" << "WHERE clustid=" << id;
+  update << "\"" << "WHERE clustid=" << id;
 
-	return update.str();
+  return update.str();
 }
